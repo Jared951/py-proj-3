b6e64d5e4030a3f4a8cd36da644272d5e1f70fb4
Jared Sewak <jaredsewak@gmail.com>
2023-05-11 23:27:23 -0600

    added shopping cart functionality, design, and ability to empty the cart

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, session, url_for
 import jinja2
 
 import melons
@@ -26,13 +26,43 @@ def melon_details(melon_id):
 @app.route("/add_to_cart/<melon_id>")
 def add_to_cart(melon_id):
    """Add a melon to the shopping cart."""
-   return f"{melon_id} added to cart"
+   if 'cart' not in session:
+      session['cart'] = {}
+
+   cart = session['cart']
+   cart[melon_id] = cart.get(melon_id, 0) + 1
+   session.modified = True
+   flash(f"Melon {melon_id} successfully added...")
+   print(cart)
+   return redirect(url_for('show_shopping_cart'))
 
 @app.route("/cart")
 def show_shopping_cart():
    """Display contents of shopping cart."""
-   flash("Welcome to the shopping cart!")
-   return render_template("cart.html")
+   order_total = 0
+   cart_melons = []
+
+   cart = session.get('cart', {})
+
+   for melon_id, quantity in cart.items():
+      melon = melons.get_by_id(melon_id)
+
+      total_cost = melon.price * quantity
+
+      order_total += total_cost
+
+      melon.quantity = quantity 
+      melon.total_cost = total_cost 
+
+      cart_melons.append(melon)
+
+   return render_template("cart.html", cart_melons=cart_melons, order_total=order_total)
+
+@app.route('/empty-cart')
+def empty_cart():
+   session['cart'] = {}
+
+   return redirect(url_for('show_shopping_cart'))
 
 if __name__ == "__main__":
     app.run(debug=True)
